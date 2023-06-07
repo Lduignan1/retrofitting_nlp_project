17443ae8a809c99ff2383ce91a76bf47945115c0
Nazanin <nazanin.shafiabadi97@gmail.com>
2023-06-07 23:47:37 +0200

    updated modified.py and lexicon.py

---
+++ lexicon.py
@@ -0,0 +1,113 @@
+import string
+from nltk.corpus import wordnet as wn
+from collections import defaultdict
+
+
+def normalize(word):
+  # words containing digits are replaced with '*NUM*'
+  if any(char.isdigit() for char in word):
+    return '*NUM*'
+  # punctuations are replaced with '*PUNC*'
+  elif word in string.punctuation:
+    return '*PUNC*'
+  # words containing symbols other than alphabetical characters, digits or punctuation marks are replaced with '*SYMBOL*'
+  elif any(char not in string.ascii_letters for char in word):
+    return '*SYMBOL*'
+  # all other words are returned intact
+  else:
+    return word 
+  
+
+class Lexicon:
+    def __init__(self, lang='eng'):
+        self.words = set()
+        self.wn_syn = {}
+        self.wn_all = {}
+        self.ppdb = defaultdict(set)    # is defaultdict better than normal dict? Why?
+        self.lang = lang
+
+    ''' WORDNET '''
+
+    ''' populate self.words with all the unique words in the WordNet library (internal method)'''
+    def _get_all_words(self):
+        if self.lang == 'eng':
+            self.words.update(wn.all_lemma_names())
+        else:
+            self.words.update(wn.all_lemma_names(lang=self.lang))
+        return self.words
+
+
+    ''' get synonymy relations (public method)'''
+    def wn_synonyms(self):
+        for word in self._get_all_words():
+            # normalize word
+            word = normalize(word)
+
+            # add a new key for the current word in the self.wn_syn dictionary
+            self.wn_syn[word] = []
+
+            synsets = wn.synsets(word, lang=self.lang)
+            for syn in synsets:
+                for lemma in syn.lemmas():
+                    if lemma.name() != word and lemma.name() not in self.wn_syn[word]: 
+                        self.wn_syn[word].append(normalize(lemma.name()))
+        
+        return self.wn_syn
+
+
+    ''' get the synonymy, hypernymy and hyponymy relations of word (public method)'''
+    def wn_all_relations(self):
+        # create a copy of the synonym relations dictionary
+        synonyms = self.wn_synonyms()
+        self.wn_all = synonyms.copy()
+        
+        # add hypernymy and hyponymy relations
+        for word in self.wn_all:
+            synsets = wn.synsets(word, lang=self.lang)
+            for synset in synsets:
+                # add hypernyms
+                for hypernym in synset.hypernyms():
+                    for lemma in hypernym.lemmas():
+                        if lemma.name() != word and lemma.name() not in self.wn_all[word]:
+                            self.wn_all[word].append(normalize(lemma.name()))
+                # add hyponyms
+                for hyponym in synset.hyponyms():
+                    for lemma in hyponym.lemmas():
+                        if lemma.name() != word and lemma.name() not in self.wn_all[word]:
+                            self.wn_all[word].append(normalize(lemma.name()))
+        
+        return self.wn_all
+
+    ''' PPDB (public method)'''
+    
+    def read_ppdb(self, filename):
+        """
+        read a file containing the paraphrase database and return a dictionary
+        input: file or path
+        output: dict with phrase terms as keys and paraphrases as values
+        """
+    
+        with open(filename, 'r', encoding='utf-8') as file:
+            for line in file:
+                line = line.split('|||')
+            
+                # strip() to remove spaces before and after words
+                self.ppdb[normalize(line[1].strip())].add(normalize(line[2].strip()))
+            
+        return self.ppdb
+    
+
+
+### Checks ###
+
+# lexicon = Lexicon()
+
+# # PPDB 
+# print(f"PPDB relations: \n{lexicon.read_ppdb('lexicons/ppdb-2.0-xl-lexical')}")
+
+# # WordNet synonyms
+# print(f"\nWordNet synonyms: \n{lexicon.wn_synonyms()}")
+
+# # WordNet all_relations
+# print(f"\nWordNet synonyms, hypernyms & hyponyms: \n{lexicon.wn_all_relations()}\n")
+
--- modified.py
+++ modified.py
@@ -1,25 +1,10 @@
-import string
 import gzip
 import numpy as np
 import sys
 import os
+from lexicon  import Lexicon # needs to be mentioned in the report
 
 from copy import deepcopy
-
-
-def normalize(word):
-  # words containing digits are replaced with '*NUM*'
-  if any(char.isdigit() for char in word):
-    return '*NUM*'
-  # punctuations are replaced with '*PUNC*'
-  elif word in string.punctuation:
-    return '*PUNC*'
-  # words containing symbols other than alphabetical characters, digits or punctuation marks are replaced with '*SYMBOL*'
-  elif any(char not in string.ascii_letters for char in word):
-    return '*SYMBOL*'
-  # all other words are returned intact
-  else:
-    return word 
 
 
 ''' Read and normalize the embeddings '''
@@ -42,20 +27,6 @@ def read_embeddings(filename):
   
   print("Reading embeddings done!")
   return embeds
-
-  
-''' Read the lexicon as a dictionary '''
-def read_lexicon(filename):
-  # keys: 1st word of each line (string)
-  # values: all the other words in the line (list of strings)
-  lexicon = {}  
-  
-  with open(filename, 'r', encoding='utf-8') as file:
-    for line in file:
-      words = line.strip().split() 
-      lexicon[normalize(words[0])] = [normalize(word) for word in words[1:]] 
-  
-  return lexicon    
 
 
 ''' Write retrofitted embeddings to specified file '''
@@ -118,14 +89,16 @@ if __name__=='__main__':
     sys.exit("\nUsage: python modified.py inFile lexicon numIter outFile\n")
   
 
-  '''Check for correct order of arguments'''
+  '''Check for correct order and format of arguments'''
   # inFile
   if not (sys.argv[1].endswith('.gz') or sys.argv[1].endswith('.txt')):
     sys.exit("\nUsage: python modified.py inFile lexicon numIter outFile\ninFile should be a .gz or .txt file.\n")
   
   # lexicon
-  if not (os.path.isfile(sys.argv[2]) or sys.argv[2].endswith('.txt')):
-    sys.exit("\nUsage: python modified.py inFile lexicon numIter outFile\nlexicon should be a .txt file.\n")
+  lex = sys.argv[2].lower()
+  if not (lex in ['wn', 'wn+', 'wordnet', 'wordnet+'] or lex == 'ppdb'):
+    sys.exit(
+      "\nUsage: python modified.py inFile lexicon numIter outFile\nPossible lexicons: PPDB or WordNet(WN) or WordNet+(WN+) (case insensitive)\n")
 
   # numIter
   if not sys.argv[3].isdigit():
@@ -142,8 +115,16 @@ if __name__=='__main__':
 
   '''if all the checks pass, proceed'''
   embeddings = read_embeddings(sys.argv[1]) # inFile
-  lexicon = read_lexicon(sys.argv[2])       # lexicon
   numIter = int(sys.argv[3])                # numIter
   outFileName = sys.argv[4]                 # outFile
+  
+  # lexicon
+  lexicon = Lexicon()                       
+  if lex == 'ppdb':
+    lexicon = lexicon.read_ppdb('lexicons/ppdb-2.0-xl-lexical')
+  elif lex in ['wn', 'wordnet']:
+    lexicon = lexicon.wn_synonyms()
+  else:
+    lexicon = lexicon.wn_all_relations()
   
   write_retrofitted_embeddings(retrofit(embeddings, lexicon, numIter), outFileName) 
